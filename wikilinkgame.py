5eabe463f39acdc2e59e92e5035e6681fba189a2
Joe Vincent <hellojustjoe@gmail.com>
2021-03-12 15:53:51 +0000

    Still tinkering with data scraping. Will be looking to complete this over the weekend.

--- wikilinkgame.py
+++ wikilinkgame.py
@@ -2,16 +2,37 @@ from bs4 import BeautifulSoup
 import requests
 
 url = input("URL: ") or "https://en.wikipedia.org/wiki/City_Literary_Institute"
+num = int(input("Enter a number: ") or 4) - 1
 
-print(url)
 
 r = requests.get(url)
-
 data = r.text
-
 soup = BeautifulSoup(data, features="lxml")
+# print(soup.prettify())
 
-print(soup.prettify())
+soup = BeautifulSoup()
+main = soup.find_all(id='mw-content-text')
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+print(main)
+
+
+# print(main)
+# print(toPrint)
+
+
+
+
+# print(main.prettify())
+
+# for link in main.find_all('a'):
+#     print(link)
+
+# linkArr = []
+# for link in soup.find_all('a'):
+#     # print(link.get('href'))
+#     # print(link)
+#     linkArr.append(link.get('href'))
+
+
+# print(linkArr[num])
+
